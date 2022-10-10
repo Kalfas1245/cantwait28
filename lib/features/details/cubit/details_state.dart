29528b46fdb1ddec65c1ee3dd69fd7787c46d880
Kamil Haras <kharas1245@gmail.com>
2022-10-10 16:45:05 +0200

    Add Details Page (fix3?)

--- lib/features/details/cubit/details_state.dart
+++ lib/features/details/cubit/details_state.dart
@@ -2,5 +2,9 @@ part of 'details_cubit.dart';
 
 class DetailsState {
   DetailsState({required this.itemModel});
+<<<<<<< HEAD
+=======
+
+>>>>>>> 6bb68880df0fd454a320f0960b6da8423548316f
   final ItemModel? itemModel;
 }
